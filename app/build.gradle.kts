924333df668027d850581177b6779501cd504348
ThalesK2005 <thales292005@gmail.com>
2025-11-21 03:54:10 -0300

    Removendo bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("kotlin-kapt") // Para o Banco de Dados Room
-    id("com.google.gms.google-services") // OBRIGATÓRIO para o Firebase funcionar
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.thales.fokus"
-    compileSdk = 34 // Usando 34 para garantir estabilidade
+    compileSdk = 34 //
 
     defaultConfig {
         applicationId = "com.thales.fokus"
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    // --- Bibliotecas Padrão ---
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -52,27 +52,27 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
-    // --- Navegação e ViewModel ---
+
     implementation("androidx.navigation:navigation-compose:2.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
 
-    // --- Room (Banco de Dados) ---
+
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
 
-    // --- Ícones Estendidos (Para o Olho da Senha, Lixeira, etc) ---
+
     implementation("androidx.compose.material:material-icons-extended:1.7.5")
 
-    // --- FIREBASE (O que estava faltando) ---
+
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
 
-    // --- Coroutines para Firebase (Resolve o erro do .await()) ---
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
-    // --- Testes ---
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
